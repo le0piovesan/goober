9d40663db1c324021dc6075b3d5a6ccfc9bcb465
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-08 15:05:09 -0300

    fix(api-posts): cleanup removed old api post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -42,9 +40,6 @@ export default function Home() {
               </div>
             </Link>
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
         </div>
       </main>
     </>

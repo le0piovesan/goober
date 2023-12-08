bdafc19d410b478ebebe6d0d5232212dafd84b21
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-08 17:28:52 -0300

    feat(root): added all router procedures

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,12 @@
+import { riderRouter } from "~/server/api/routers/rider";
+import { driverRouter } from "~/server/api/routers/driver";
+import { rideRouter } from "~/server/api/routers/ride";
 import { createTRPCRouter } from "~/server/api/trpc";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  rider: riderRouter,
+  driver: driverRouter,
+  ride: rideRouter,
+});
 
-// export type definition of API
 export type AppRouter = typeof appRouter;

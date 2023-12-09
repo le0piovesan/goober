d85137fe2f5057a02959b0347b9bb1fa129dee95
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-09 11:43:25 -0300

    chore(tailwind.config): add initial application colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        primary: "#4ffbdf",
+        secondary: "#845ec2",
+        optional: "#2ac97a",
+        background: "#8888dd",
+        border: "#3f48cc",
+      },
     },
   },
   plugins: [],

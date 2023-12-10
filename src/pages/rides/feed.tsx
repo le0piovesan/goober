d901d98f3200544c8aa80aa1989cb375710d6f20
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-10 01:10:49 -0300

    feat(feed): basic feed implementation

--- src/pages/rides/feed.tsx
+++ src/pages/rides/feed.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { type NextPage } from "next";
+import { useContext } from "react";
+import { AuthContext } from "~/context/AuthContext";
 
-const Feed: React.FC = () => <div>Feed</div>;
+const Feed: NextPage = () => {
+  const { logout } = useContext(AuthContext);
+
+  const handleLogOut = async () => {
+    await logout();
+  };
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold">Feed</h1>
+      <button onClick={() => handleLogOut()}>Log out</button>
+    </div>
+  );
+};
 
 export default Feed;

fd77cf8e0cbae5895c8d9ebeaca5c3ef8dd034ac
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-10 01:09:36 -0300

    feat(auth router): create auth router for login procedure

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,11 +2,13 @@ import { riderRouter } from "~/server/api/routers/rider";
 import { driverRouter } from "~/server/api/routers/driver";
 import { rideRouter } from "~/server/api/routers/ride";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { authRouter } from "./routers/auth";
 
 export const appRouter = createTRPCRouter({
   rider: riderRouter,
   driver: driverRouter,
   ride: rideRouter,
+  auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/auth.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import bcrypt from "bcrypt";
+
+export const authRouter = createTRPCRouter({
+  login: publicProcedure
+    .input(z.object({ email: z.string(), password: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const existingDriver = await ctx.db.driver.findUnique({
+        where: { email: input.email },
+      });
+
+      const existingRider = await ctx.db.rider.findUnique({
+        where: { email: input.email },
+      });
+
+      const user = existingDriver ?? existingRider;
+
+      if (!user) {
+        throw new Error("No user with this email exists");
+      }
+
+      if (!bcrypt.compareSync(input.password, user.password)) {
+        throw new Error("Incorrect password");
+      }
+      return user;
+    }),
+});

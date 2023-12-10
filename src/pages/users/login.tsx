e03ee158d95be233c8aa303dc7547f60a56c9a4f
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-10 01:11:52 -0300

    feat(login): simple login page with some validations

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,5 +1,83 @@
-import React from "react";
+import { type NextPage } from "next";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { api } from "~/utils/api";
+import { useContext, useState } from "react";
+import { AuthContext } from "~/context/AuthContext";
+import useAuthRedirect from "~/hooks/useAuthRedirect";
 
-const Login: React.FC = () => <div>Login</div>;
+const schema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string(),
+});
+
+type FormInputsProps = z.infer<typeof schema>;
+
+const Login: NextPage = () => {
+  const { isLoggedIn, redirectTo } = useAuthRedirect();
+  isLoggedIn ? redirectTo("/rides/feed") : redirectTo("/users/login");
+
+  const [submitError, setSubmitError] = useState<string | null>(null);
+  const { login } = useContext(AuthContext);
+  const auth = api.auth.login.useMutation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputsProps>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormInputsProps> = async (data) => {
+    try {
+      const { email, password } = data;
+      const response = await auth.mutateAsync({ email, password });
+      await login(response.id);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) setSubmitError(error.message);
+      else setSubmitError("An unknown error occurred");
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto w-full max-w-md space-y-4"
+      >
+        <input
+          {...register("email")}
+          placeholder="Email"
+          type="email"
+          className="w-full rounded-md border px-3 py-2"
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+
+        <input
+          {...register("password")}
+          placeholder="Password"
+          type="password"
+          className="w-full rounded-md border px-3 py-2"
+        />
+
+        {submitError && <p className="text-red-500">{submitError}</p>}
+
+        <button
+          type="submit"
+          className="w-full cursor-pointer rounded-md bg-blue-500 px-3 py-2 text-white hover:bg-blue-600"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;

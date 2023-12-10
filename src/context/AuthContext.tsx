558182a9160a62a88bce1fcb8a36859ecfd4e8ef
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-10 01:10:29 -0300

    feat(AuthContext): changes in context behavior

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,11 @@
+import { useRouter } from "next/router";
 import { createContext, useState, type ReactNode } from "react";
 
 type AuthContextType = {
-  userId: string | null;
+  userId: number | null;
   isLoggedIn: boolean;
-  login: (userId: string) => void;
-  logout: () => void;
+  login: (userId: number) => Promise<void>;
+  logout: () => Promise<void>;
 };
 
 type AuthContextProviderProps = {
@@ -23,17 +24,21 @@ export const AuthContext = createContext<AuthContextType>({
 });
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
-  const [userId, setUserId] = useState<string | null>(null);
+  const [userId, setUserId] = useState<number | null>(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
 
-  const login = (userId: string) => {
+  const login = async (userId: number) => {
+    console.log("Logging in with user id: ", userId);
     setUserId(userId);
     setIsLoggedIn(true);
+    await router.push("/rides/feed");
   };
 
-  const logout = () => {
+  const logout = async () => {
     setUserId(null);
     setIsLoggedIn(false);
+    await router.push("/");
   };
 
   return (

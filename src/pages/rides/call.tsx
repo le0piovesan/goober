6b9c836197b777ac6c4824e20ebf01cd69f25729
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-11 19:22:54 -0300

    feat(call page): rendering the map component

--- src/pages/rides/call.tsx
+++ src/pages/rides/call.tsx
@@ -1,8 +1,15 @@
-import { Heading } from "@chakra-ui/react";
 import { type NextPage } from "next";
+import ContainerForm from "~/components/ContainerForm";
+import Map from "~/components/Map";
+import { useJsApiLoader } from "@react-google-maps/api";
+import Loading from "~/components/Loading";
 
 const Call: NextPage = () => {
-  return <Heading>Call a Goober Driver</Heading>;
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
+    libraries: ["places"],
+  });
+  return <ContainerForm>{isLoaded ? <Map /> : <Loading />}</ContainerForm>;
 };
 
 export default Call;

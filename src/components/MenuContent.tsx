114be69387d67f4f3f9dd2e705db8a32ee52e4ec
le0piovesan <leopvsn@gmail.com>
2023-12-10 23:19:53 -0300

    feat(Menu|User content): using persistant logged in menu

--- src/components/MenuContent.tsx
+++ src/components/MenuContent.tsx
@@ -1,28 +1,55 @@
 import { Flex } from "@chakra-ui/react";
 import { FiMap, FiMapPin, FiBell, FiLogOut } from "react-icons/fi";
 import ButtonComponent from "./ButtonComponent";
+import { useAuth } from "~/context/AuthContext";
+import { useRouter } from "next/router";
 
-const MenuContent = () => (
-  <Flex
-    h="100vh"
-    align={"flex-start"}
-    direction={"column"}
-    pr={4}
-    overflow="hidden"
-  >
-    <ButtonComponent outline leftIcon={<FiMap size={24} />}>
-      Home
-    </ButtonComponent>
-    <ButtonComponent outline leftIcon={<FiMapPin size={24} />}>
-      Find Goober Driver
-    </ButtonComponent>
-    <ButtonComponent outline leftIcon={<FiBell size={24} />}>
-      Notifications
-    </ButtonComponent>
-    <ButtonComponent outline leftIcon={<FiLogOut size={24} />}>
-      Log Out
-    </ButtonComponent>
-  </Flex>
-);
+const MenuContent = () => {
+  const { logout } = useAuth();
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  const menuItems = [
+    { href: "/rides/feed", icon: <FiMap size={24} />, label: "Home" },
+    {
+      href: "/rides/call",
+      icon: <FiMapPin size={24} />,
+      label: "Find Goober Driver",
+    },
+    {
+      href: "/notifications/list",
+      icon: <FiBell size={24} />,
+      label: "Notifications",
+    },
+  ];
+
+  return (
+    <Flex
+      h="100vh"
+      align={"flex-start"}
+      direction={"column"}
+      pr={4}
+      overflow="hidden"
+    >
+      {menuItems.map((item) => (
+        <ButtonComponent
+          key={item.href}
+          href={currentPath === item.href ? "#" : item.href}
+          outline
+          leftIcon={item.icon}
+        >
+          {item.label}
+        </ButtonComponent>
+      ))}
+      <ButtonComponent
+        onClick={() => logout()}
+        outline
+        leftIcon={<FiLogOut size={24} />}
+      >
+        Log Out
+      </ButtonComponent>
+    </Flex>
+  );
+};
 
 export default MenuContent;
--- src/components/UserContent.tsx
+++ src/components/UserContent.tsx
@@ -1,10 +1,14 @@
 import { Flex, Box } from "@chakra-ui/react";
+import { useAuth } from "~/context/AuthContext";
+import Layout from "./Layout";
 
 const UserContent = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useAuth();
+
   return (
     <Flex direction="row" align="start" className="container mx-auto">
       <Box flexGrow={1} minHeight="100vh">
-        {children}
+        {user && user.isLoggedIn ? <Layout>{children}</Layout> : children}
       </Box>
     </Flex>
   );

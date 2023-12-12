1d1093a94d709971fc716ab0dabdd6861848b2f1
le0piovesan <leopvsn@gmail.com>
2023-12-12 01:04:18 -0300

    feat(map): basic map implementation with details

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,17 @@
-import { GoogleMap } from "@react-google-maps/api";
+import { GoogleMap, Marker, DirectionsRenderer } from "@react-google-maps/api";
 import InputComponent from "./InputComponent";
 import ButtonComponent from "~/components/ButtonComponent";
-import { Card, CardBody, VStack, useToast } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Stack,
+  VStack,
+  Heading,
+  Text,
+  useToast,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
 import { api } from "~/utils/api";
 import { useAuth } from "~/context/AuthContext";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -10,20 +20,36 @@ import { useForm, type SubmitHandler } from "react-hook-form";
 import { useLoading } from "~/hooks/useLoading";
 import { Autocomplete } from "@react-google-maps/api";
 import useCurrentPosition from "~/hooks/useCurrentPosition";
+import { useRef, useState } from "react";
 
 const schema = z.object({
   pickupLocation: z.string(),
   dropoffLocation: z.string(),
 });
 
+type Coordinates = {
+  lat: number;
+  lng: number;
+};
+
 type FormInputsProps = z.infer<typeof schema>;
 
 const Map: React.FC = () => {
+  console.log("Map Component Mounted");
   const { user } = useAuth();
   const { position } = useCurrentPosition();
   const ride = api.ride.createRide.useMutation();
   const toast = useToast();
   const { loading, startLoading, stopLoading } = useLoading();
+  const pickupLocationRef = useRef<Coordinates | null>(null);
+  const dropoffLocationRef = useRef<Coordinates | null>(null);
+  const [distanceDetails, setDistanceDetails] = useState({
+    value: 0,
+    distance: "",
+  });
+  const [directions, setDirections] = useState<
+    google.maps.DirectionsResult | undefined
+  >();
 
   const {
     register,
@@ -36,6 +62,36 @@ const Map: React.FC = () => {
       dropoffLocation: "",
     },
   });
+
+  const getInfoLocation = async () => {
+    startLoading();
+    const directionService = new google.maps.DirectionsService();
+
+    if (!pickupLocationRef.current || !dropoffLocationRef.current) {
+      console.error("Pickup or dropoff location is not set");
+      return;
+    }
+
+    try {
+      const result = await directionService.route({
+        origin: pickupLocationRef.current,
+        destination: dropoffLocationRef.current,
+        travelMode: google.maps.TravelMode.DRIVING,
+      });
+
+      setDirections(result);
+      const legs = result.routes[0]?.legs[0];
+
+      setDistanceDetails({
+        value: legs?.distance?.value ?? 0,
+        distance: legs?.distance?.text ?? "",
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      stopLoading();
+    }
+  };
 
   const onSubmit: SubmitHandler<FormInputsProps> = async (data) => {
     startLoading();
@@ -59,13 +115,35 @@ const Map: React.FC = () => {
   return (
     <GoogleMap
       zoom={15}
-      mapContainerStyle={{ width: "100%", height: "100vh" }}
+      mapContainerStyle={{ width: "100%", height: "80vh" }}
       center={{
         lat: position.latitude,
         lng: position.longitude,
       }}
     >
-      <Card bgColor={"transparent"}>
+      {pickupLocationRef.current && dropoffLocationRef.current && (
+        <>
+          <Marker position={pickupLocationRef.current} />
+          <Marker position={dropoffLocationRef.current} />
+          <DirectionsRenderer
+            directions={directions}
+            options={{
+              polylineOptions: {
+                strokeColor: "#845ec2",
+                strokeWeight: 5,
+              },
+            }}
+          />
+        </>
+      )}
+      <Card
+        bgColor={"light"}
+        position="absolute"
+        bottom="5%"
+        left="5%"
+        boxShadow="0px 4px 20px rgba(0, 0, 0, 0.5)"
+        borderRadius="10px"
+      >
         <CardBody>
           <VStack
             as="form"
@@ -74,26 +152,97 @@ const Map: React.FC = () => {
             w="full"
             maxW="md"
           >
-            <Autocomplete>
-              <InputComponent
-                label="Get a ride"
-                register={register}
-                name="pickupLocation"
-                placeholder="Origin"
-              />
-            </Autocomplete>
-            <Autocomplete>
-              <InputComponent
-                label="Where to?"
-                register={register}
-                name="dropoffLocation"
-                placeholder="Destination"
-              />
-            </Autocomplete>
+            {!distanceDetails.distance ||
+            (!distanceDetails.value && !directions) ? (
+              <>
+                <Autocomplete
+                  onLoad={(autocomplete) => {
+                    autocomplete.addListener("place_changed", () => {
+                      const place = autocomplete.getPlace();
+                      const lat = place.geometry?.location?.lat();
+                      const lng = place.geometry?.location?.lng();
 
-            <ButtonComponent type="submit" loading={loading}>
-              Find Goober Driver
-            </ButtonComponent>
+                      pickupLocationRef.current = {
+                        lat: lat ?? 0,
+                        lng: lng ?? 0,
+                      };
+                    });
+                  }}
+                >
+                  <InputComponent
+                    label="Get a ride"
+                    register={register}
+                    name="pickupLocation"
+                    placeholder="Origin"
+                  />
+                </Autocomplete>
+                <Autocomplete
+                  onLoad={(autocomplete) => {
+                    autocomplete.addListener("place_changed", () => {
+                      const place = autocomplete.getPlace();
+                      const lat = place.geometry?.location?.lat();
+                      const lng = place.geometry?.location?.lng();
+
+                      dropoffLocationRef.current = {
+                        lat: lat ?? 0,
+                        lng: lng ?? 0,
+                      };
+                    });
+                  }}
+                >
+                  <InputComponent
+                    label="Where to?"
+                    register={register}
+                    name="dropoffLocation"
+                    placeholder="Destination"
+                  />
+                </Autocomplete>
+
+                <ButtonComponent
+                  onClick={() => getInfoLocation()}
+                  loading={loading}
+                >
+                  Search 🔍
+                </ButtonComponent>
+              </>
+            ) : (
+              <Stack>
+                <Flex align={"center"}>
+                  <Box p={2}>
+                    <Heading size="sm" fontWeight={""}>
+                      Distance
+                    </Heading>
+                    <Text fontSize="md" fontWeight={"bold"} color={"primary"}>
+                      {distanceDetails.distance}
+                    </Text>
+                  </Box>
+
+                  <Box p={2}>
+                    <Heading size="sm" fontWeight={""}>
+                      Value
+                    </Heading>
+                    <Text fontSize="md" fontWeight={"bold"} color={"green"}>
+                      ${" "}
+                      {Number((distanceDetails.value / 1000) * 0.32).toFixed(2)}
+                    </Text>
+                  </Box>
+                </Flex>
+                <ButtonComponent type="submit" loading={loading}>
+                  Request Goober Driver
+                </ButtonComponent>
+                <ButtonComponent
+                  textOnly
+                  onClick={() => {
+                    setDistanceDetails({
+                      value: 0,
+                      distance: "",
+                    });
+                  }}
+                >
+                  Go back
+                </ButtonComponent>
+              </Stack>
+            )}
           </VStack>
         </CardBody>
       </Card>
--- src/pages/rides/call.tsx
+++ src/pages/rides/call.tsx
@@ -1,15 +1,17 @@
 import { type NextPage } from "next";
-import ContainerForm from "~/components/ContainerForm";
 import Map from "~/components/Map";
-import { useJsApiLoader } from "@react-google-maps/api";
+import { useJsApiLoader, type Libraries } from "@react-google-maps/api";
 import Loading from "~/components/Loading";
+import { useRef } from "react";
 
 const Call: NextPage = () => {
+  const libraries = useRef<Libraries>(["places"]);
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
-    libraries: ["places"],
+    libraries: libraries.current,
   });
-  return <ContainerForm>{isLoaded ? <Map /> : <Loading />}</ContainerForm>;
+  return <>{isLoaded ? <Map /> : <Loading />}</>;
 };
 
 export default Call;

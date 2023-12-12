8d6c9409428e290ab2410890ecf15148e95ec340
le0piovesan <leopvsn@gmail.com>
2023-12-12 11:22:36 -0300

    feat(notification router): initial notification procedure

--- src/server/api/routers/notification.ts
+++ src/server/api/routers/notification.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { getDistance } from "geolib";
 
 export const notificationRouter = createTRPCRouter({
   getNotifications: publicProcedure
@@ -12,5 +13,55 @@ export const notificationRouter = createTRPCRouter({
       });
 
       return notification;
+    }),
+
+  requestClosestDriver: publicProcedure
+    .input(
+      z.object({
+        rideId: z.number(),
+        pickupLocation: z.object({
+          latitude: z.number(),
+          longitude: z.number(),
+        }),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const drivers = await ctx.db.driver.findMany({
+        where: {
+          onTrip: false,
+          declinedRides: {
+            none: {
+              rideId: input.rideId,
+            },
+          },
+        },
+        include: { lastLocation: true },
+      });
+
+      //
+      const driversWithDistance = drivers.map((driver) => ({
+        ...driver,
+        distance: getDistance(
+          {
+            latitude: input.pickupLocation.latitude,
+            longitude: input.pickupLocation.longitude,
+          },
+          {
+            latitude: driver.lastLocation.latitude,
+            longitude: driver.lastLocation.longitude,
+          },
+        ),
+      }));
+
+      driversWithDistance.sort((a, b) => a.distance - b.distance);
+
+      const closestDriver = driversWithDistance[0];
+
+      // const notification = await sendNotification(
+      //   closestDriver,
+      //   "You have a new ride request",
+      // );
+
+      // return notification;
     }),
 });

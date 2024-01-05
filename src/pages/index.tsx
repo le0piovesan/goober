d6b90896344a6b9c845b72a5099968b33228ccf1
le0piovesan <leopvsn@gmail.com>
2024-01-04 23:49:37 -0300

    fix(index): redirect flow according to user in application storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { Flex, Heading, VStack } from "@chakra-ui/react";
 import { type NextPage } from "next";
+import { useEffect } from "react";
 import ButtonComponent from "~/components/ButtonComponent";
 import TitleComponent from "~/components/TitleComponent";
+import { useAuth } from "~/context/AuthContext";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user && user.isLoggedIn) void router.replace("/rides/feed");
+  }, []);
+
   return (
     <Flex direction="column" align="center" justify="center" h="100vh">
       <VStack align="start" p={4} fontSize="5xl" fontWeight="bold">

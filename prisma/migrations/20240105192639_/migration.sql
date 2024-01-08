51cbd9e2035c07fb825ec10cfbd7834a513c8904
le0piovesan <leopvsn@gmail.com>
2024-01-08 00:09:31 -0300

    chore: remove unnecessary migrations file

--- prisma/migrations/20240105192639_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Driver" ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'Regular';
-
--- AlterTable
-ALTER TABLE "Ride" ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'Regular';
